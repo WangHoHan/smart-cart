32d7317af5aaa05c380441dd17106d3328ba3b49
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2021-04-09 22:49:58 +0200

    buggy

--- graph.py
+++ graph.py
@@ -1,5 +1,96 @@
-@staticmethod
+import tractor
+class Node:
+    def __init__(self, action, direction, parent, x, y):
+        self.action = action
+        self.direction = direction
+        self.parent = parent
+        self.x = x
+        self.y = y
+    def get_action(self):
+        return self.action
+    def set_action(self, action):
+        self.action = action
+    def get_direction(self):
+        return self.direction
+    def set_direction(self, direction):
+        self.direction = direction
+    def get_parent(self):
+        return self.parent
+    def set_parent(self, parent):
+        self.parent = parent
+    def get_x(self):
+        return self.x
+    def set_x(self, x):
+        self.x = x
+    def get_y(self):
+        return self.y
+    def set_y(self, y):
+        self.y = y
+class Istate: #stan początkowy traktora
+    def __init__(self, direction, x, y):
+        self.direction = direction
+        self.x = x
+        self.y = y
+    def get_direction(self):
+        return self.direction
+    def set_direction(self, direction):
+        self.direction = direction
+    def get_x(self):
+        return self.x
+    def set_x(self, x):
+        self.x = x
+    def get_y(self):
+        return self.y
+    def set_y(self, y):
+        self.y = y
+# class Fringe: #kolejka zawierająca akcje oraz pola do odwiedzenia
+#     def __init__(self, fringe):
+#         self.fringe = fringe
+#     def get_fringe(self):
+#         return self.fringe
+#     def set_fringe(self, fringe):
+#         self.fringe = fringe
+#     def add_to_fringe(self, value):
+#         self.fringe.append(value)
+#     def get_element_from_fringe_pop(self):
+#         return self.fringe.pop(0)
+def goal_test(elem, goaltest):
+    if elem.get_x() == goaltest.get_x() and elem.get_y() == goaltest.get_y(): #goaltest bez getterów
+        return True
+    else:
+        return False
+def succ(elem):
+    actions_list = []
+    actions_list.append(("rotate_left", (elem.get_x(), elem.get_y())))
+    actions_list.append(("rotate_right", (elem.get_x(), elem.get_y())))
+    if tractor.Tractor.is_move_allowed_succ(elem) == "x + 1":
+        actions_list.append(("move", (elem.set_x(elem.get_x() + 1), elem.get_y())))
+    elif tractor.Tractor.is_move_allowed_succ(elem) == "y - 1":
+        actions_list.append(("move", (elem.get_x(), elem.set_y(elem.get_y() - 1))))
+    elif tractor.Tractor.is_move_allowed_succ(elem) == "y + 1":
+        actions_list.append(("move", (elem.set_x(elem.get_x()), elem.set_y(elem.get_y() + 1))))
+    elif tractor.Tractor.is_move_allowed_succ(elem) == "x - 1":
+        actions_list.append(("move", (elem.set_x(elem.get_x() - 1), elem.get_y())))
+    return actions_list
 def graphsearch(fringe, explored, istate, succ, goaltest):
-    fringe.add_to_fringe(istate)
+    node = Node(None, istate.get_direction(), None, istate.get_x(), istate.get_y())
+    #fringe.add_to_fringe(node)
+    fringe.append(node)
     while True:
-        
+        if not fringe:
+            return False
+        #elem = fringe.get_element_from_fringe_pop()
+        elem = fringe.pop(0)
+        if goal_test(elem, goaltest) is True:
+            return explored
+        explored.append(elem)
+        for (action, state) in succ(elem):
+            if state not in fringe and state not in explored:
+                x = Node(action, elem.get_direction(), elem, state[0], state[1])
+                print(state[0])
+                print(state[1])
+                print(x.get_action())
+                print(x.get_direction())
+                print(x.get_parent())
+
+                fringe.append(x)
--- py.py
+++ py.py
@@ -1,5 +1,5 @@
 import definitions
-import fringe
+import graph
 import map
 import plant
 import pygame
@@ -8,14 +8,13 @@ import tractor
 pygame.display.set_caption("Smart Tractor")
 def main():
     #tworzenie podstawowych obiektów
-    fringe1 = fringe.Fringe([])
     map1 = map.Map([])
     map1.create_base_map()
     amount_of_seeds_dict = {"beetroot": definitions.TRACTOR_AMOUNT_OF_SEEDS_EACH_TYPE, "carrot": definitions.TRACTOR_AMOUNT_OF_SEEDS_EACH_TYPE, "potato": definitions.TRACTOR_AMOUNT_OF_SEEDS_EACH_TYPE, "wheat": definitions.TRACTOR_AMOUNT_OF_SEEDS_EACH_TYPE}
     collected_plants_dict = {"beetroot": 0, "carrot": 0, "potato": 0, "wheat": 0}
     fertilizer_dict = {"beetroot": definitions.TRACTOR_FERTILIZER, "carrot": definitions.TRACTOR_FERTILIZER, "potato": definitions.TRACTOR_FERTILIZER, "wheat": definitions.TRACTOR_FERTILIZER}
     station1 = station.Station(collected_plants_dict)
-    tractor1 = tractor.Tractor(amount_of_seeds_dict, collected_plants_dict, definitions.TRACTOR_DIRECTION_NORTH, fertilizer_dict, definitions.TRACTOR_FUEL, definitions.TRACTOR_WATER_LEVEL, 0, 0)
+    tractor1 = tractor.Tractor(amount_of_seeds_dict, collected_plants_dict, definitions.TRACTOR_DIRECTION_SOUTH, fertilizer_dict, definitions.TRACTOR_FUEL, definitions.TRACTOR_WATER_LEVEL, 0, 0)
     tractor1_rect = pygame.Rect(tractor1.get_x(), tractor1.get_y(), definitions.BLOCK_SIZE, definitions.BLOCK_SIZE)
     clock = pygame.time.Clock()
     run = True
@@ -24,6 +23,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        istate = graph.Istate(definitions.TRACTOR_DIRECTION_SOUTH, 0, 0)
+        istate1 = graph.Istate(None, 4, 4)
+        print(graph.graphsearch([], [], istate, graph.succ, istate1))
         map1.draw_window(tractor1, tractor1_rect)
         tractor1.tractor1_handle_movement(tractor1_rect)
         tractor1.do_work(map1, station1, tractor1_rect)
